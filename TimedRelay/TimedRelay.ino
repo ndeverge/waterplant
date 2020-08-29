d2f8f68f5553013aa0e44a29f6a1b7e37c10b4dc
Nicolas Deverge <ndeverge@ekito.fr>
2020-08-29 15:13:13 +0200

    Compute time left before watering

--- TimedRelay/TimedRelay.ino
+++ TimedRelay/TimedRelay.ino
@@ -10,17 +10,22 @@
 
 #define HOUR 60*MINUTE
 
+#define DAY 24UL*HOUR
+
 int buttonState = 0;         // variable for reading the pushbutton status
+AlarmId id;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+
+  Serial.begin(9600);
 
   pinMode(BUTTON, INPUT); // initialize the pushbutton pin as an input:
   
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(RELAY, OUTPUT);
 
-  Alarm.timerRepeat(24UL*HOUR, watering); // called once after XXX seconds 
+  id = Alarm.timerRepeat(DAY, watering); // called once after XXX seconds 
 }
 
 // the loop function runs over and over again forever
@@ -35,6 +40,8 @@ void loop() {
       watering();
   }
 
+  digitalClockDisplay();
+
   Alarm.delay(100); // wait a bit
 }
 
@@ -47,4 +54,22 @@ void watering() {
 
   Alarm.delay(duration);
   digitalWrite(RELAY, LOW);   // turn off the RELAY
+}
+
+time_t time;
+void digitalClockDisplay() {
+  // digital clock display of the time
+
+  time = DAY - now();
+  Serial.print(hour(time));
+  printDigits(minute(time));
+  printDigits(second(time));
+  Serial.println();
+}
+
+void printDigits(int digits) {
+  Serial.print(":");
+  if (digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
 }
