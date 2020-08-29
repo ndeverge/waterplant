fb04d2149ffa9a3d3e287c58fc804ee1c032f376
Nicolas Deverge <ndeverge@ekito.fr>
2020-08-29 17:39:44 +0200

    Display watering time left

--- TimedRelay/TimedRelay.ino
+++ TimedRelay/TimedRelay.ino
@@ -11,6 +11,12 @@
 
 DHT dht(DHTPIN, DHTTYPE);
 
+#define MOISTUREPIN A0  //A0 on breakboard
+// Moisture values (http://wiki.seeedstudio.com/Grove-Moisture_Sensor/)
+// Sensor in dry soil     Min: 0      Max:300
+// Sensor in humid soil   Min: 300    Max:700
+// Sensor in water 0      Min: 700    Max:950
+
 #define BUTTON 2      //D2 on breakboard
 
 #define RELAY 4  //D4 on breakboard
@@ -21,13 +27,17 @@ DHT dht(DHTPIN, DHTTYPE);
 
 #define DAY 24UL*HOUR
 
+#define WATERING_DURATION 10*MINUTE
+
 int buttonState = 0;         // variable for reading the pushbutton status
 AlarmId id;
+short watering = 0;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
 
   Serial.begin(9600);
+  Serial.println("Initializing");
 
   Wire.begin();
 
@@ -43,8 +53,10 @@ void setup() {
   
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(RELAY, OUTPUT);
+  
+  pinMode(MOISTUREPIN, INPUT);
 
-  id = Alarm.timerRepeat(DAY, watering); // called once after XXX seconds 
+  id = Alarm.timerRepeat(DAY, startWatering); // called once after XXX seconds 
 }
 
 // the loop function runs over and over again forever
@@ -56,11 +68,44 @@ void loop() {
   // check if the pushbutton is pressed.
   // if it is, the buttonState is HIGH:
   if (buttonState == HIGH) {
-      watering();
+      startWatering();
   }
 
-  displayTimeLeftBeforeWatering();
+  if (watering == 0) {
+    displayTimeLeftBeforeWatering();
+  } else {
+    displayTimeLeftDuringWatering();
+  }
+  
+  displayTempAndHumidity();
 
+  displayMoisture();
+
+  Alarm.delay(500); // wait a bit
+}
+
+void startWatering() {
+  if (watering == 0) {
+
+    SeeedOled.clearDisplay();
+  
+    Serial.println("Starting watering");
+    
+    watering = 1;
+    digitalWrite(RELAY, HIGH);   // turn the RELAY on (HIGH is the voltage level)
+  
+    Alarm.timerOnce(WATERING_DURATION, stopWatering);
+  }
+}
+
+void stopWatering() {
+  Serial.println("Stopping watering");
+  
+  digitalWrite(RELAY, LOW);   // turn off the RELAY
+  watering = 0;
+}
+
+void displayTempAndHumidity() {
   float temp_hum_val[2] = {0};
 
   if(!dht.readTempAndHumidity(temp_hum_val)){
@@ -77,41 +122,39 @@ void loop() {
   } else {
      Serial.println("Failed to get temprature and humidity value.");
   }
-
-  Alarm.delay(500); // wait a bit
 }
 
-void watering() {
+void displayMoisture() {
+  int moisture = analogRead(MOISTUREPIN);
 
-  // 10mn
-  const unsigned long duration = 10UL * (60UL * 1000UL);//ms
+  // Convert it to a percentage
+  moisture = map(moisture, 0, 1023, 0, 100);
   
-  digitalWrite(RELAY, HIGH);   // turn the RELAY on (HIGH is the voltage level)
-
-  Alarm.delay(duration);
-  digitalWrite(RELAY, LOW);   // turn off the RELAY
+  SeeedOled.setTextXY(7, 0);
+  SeeedOled.putString("Moisture: "); 
+  SeeedOled.putFloat(moisture);
+  SeeedOled.putString("%"); 
 }
 
-time_t time;
+void displayTimeLeftDuringWatering() {
+  SeeedOled.setTextXY(0, 0);
+  SeeedOled.putString("Arrosage pendant");
+
+long timeLeft = WATERING_DURATION - now();
+  Serial.println(timeLeft);
+  
+  SeeedOled.setTextXY(1, 1);
+  printDigitsOled(minute(timeLeft));
+  SeeedOled.putString(" minutes");
+  SeeedOled.setTextXY(2, 1);
+  printDigitsOled(second(timeLeft));
+  SeeedOled.putString(" secondes");
+}
+
 void displayTimeLeftBeforeWatering() {
-  // digital clock display of the time
 
-  time = DAY - now();
-  
-  printOnSerialPort();
+  time_t time = DAY - now();
 
-  printOnOled();
-
-}
-
-void printOnSerialPort() {
-  Serial.print(hour(time));
-  printDigitsSerial(minute(time));
-  printDigitsSerial(second(time));
-  Serial.println();
-}
-
-void printOnOled() {
   SeeedOled.setTextXY(0, 0);
   SeeedOled.putString("Demarrage dans");
   SeeedOled.setTextXY(1, 1);
@@ -123,13 +166,6 @@ void printOnOled() {
   SeeedOled.setTextXY(3, 1);
   printDigitsOled(second(time));
   SeeedOled.putString(" secondes");
-}
-
-void printDigitsSerial(int digits) {
-  Serial.print(":");
-  if (digits < 10)
-    Serial.print('0');
-  Serial.print(digits);
 }
 
 void printDigitsOled(int digits) {
