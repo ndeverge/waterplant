9999da348d6b0befff833bf663738c9b5a7fc0b9
Nicolas Deverge <ndeverge@ekito.fr>
2020-09-05 16:15:02 +0200

    Transfer water

--- TimedRelay/TimedRelay.ino
+++ TimedRelay/TimedRelay.ino
@@ -17,9 +17,11 @@ DHT dht(DHTPIN, DHTTYPE);
 // Sensor in humid soil   Min: 300    Max:700
 // Sensor in water 0      Min: 700    Max:950
 
-#define BUTTON 2      //D2 on breakboard
+#define BUTTON_1 2      //D2 on breakboard
+#define BUTTON_2 6      //D6 on breakboard
 
-#define RELAY 4  //D4 on breakboard
+#define RELAY_1 4  //D4 on breakboard
+#define RELAY_2 5  // see example at https://wiki.seeedstudio.com/Grove-2-Channel_SPDT_Relay/
 
 #define MINUTE 60
 
@@ -29,8 +31,10 @@ DHT dht(DHTPIN, DHTTYPE);
 
 #define WATERING_DURATION 10*MINUTE
 
-int buttonState = 0;         // variable for reading the pushbutton status
-AlarmId id;
+#define TRANSFER_DURATION 1*MINUTE
+
+int buttonState_1 = 0;         // variable for reading the pushbutton status
+int buttonState_2 = 0;
 short watering = 0;
 
 // the setup function runs once when you press reset or power the board
@@ -49,27 +53,33 @@ void setup() {
   SeeedOled.setNormalDisplay();      //Set display to normal mode (i.e non-inverse mode)
   SeeedOled.setPageMode();   
 
-  pinMode(BUTTON, INPUT); // initialize the pushbutton pin as an input:
+  pinMode(BUTTON_1, INPUT); // initialize the pushbutton pin as an input:
+  pinMode(BUTTON_2, INPUT); // initialize the pushbutton pin as an input:
   
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(RELAY, OUTPUT);
+  pinMode(RELAY_1, OUTPUT);
+  pinMode(RELAY_2, OUTPUT);
   
   pinMode(MOISTUREPIN, INPUT);
 
-  id = Alarm.timerRepeat(DAY, startWatering); // called once after XXX seconds 
+  Alarm.timerRepeat(DAY, startWatering); // called once after XXX seconds 
 }
 
 // the loop function runs over and over again forever
 void loop() {
 
   // read the state of the pushbutton value:
-  buttonState = digitalRead(BUTTON);
+  buttonState_1 = digitalRead(BUTTON_1);
+  buttonState_2 = digitalRead(BUTTON_2);
 
   // check if the pushbutton is pressed.
   // if it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
+  if (buttonState_1 == HIGH) {
       startWatering();
   }
+
+  if (buttonState_2 == HIGH) {
+    startTransfer();
+  } 
 
   if (watering == 0) {
     displayTimeLeftBeforeWatering();
@@ -84,6 +94,12 @@ void loop() {
   Alarm.delay(500); // wait a bit
 }
 
+void scheduledWatering() {
+
+  setTime(0, 0, 0, 1, 1, 2020);
+  startWatering();
+}
+
 void startWatering() {
   if (watering == 0) {
 
@@ -92,7 +108,7 @@ void startWatering() {
     Serial.println("Starting watering");
     
     watering = 1;
-    digitalWrite(RELAY, HIGH);   // turn the RELAY on (HIGH is the voltage level)
+    digitalWrite(RELAY_1, HIGH);   // turn the RELAY on (HIGH is the voltage level)
   
     Alarm.timerOnce(WATERING_DURATION, stopWatering);
   }
@@ -101,8 +117,21 @@ void startWatering() {
 void stopWatering() {
   Serial.println("Stopping watering");
   
-  digitalWrite(RELAY, LOW);   // turn off the RELAY
+  digitalWrite(RELAY_1, LOW);   // turn off the RELAY
   watering = 0;
+}
+
+void startTransfer() {
+  Serial.println("Start transfer");
+  digitalWrite(RELAY_2, HIGH);   // turn the RELAY on (HIGH is the voltage level)
+
+  Alarm.timerOnce(TRANSFER_DURATION, stopTransfer);
+}
+
+void stopTransfer() {
+  Serial.println("Stopping transfer");
+  
+  digitalWrite(RELAY_2, LOW);   // turn off the RELAY
 }
 
 void displayTempAndHumidity() {
@@ -140,8 +169,8 @@ void displayTimeLeftDuringWatering() {
   SeeedOled.setTextXY(0, 0);
   SeeedOled.putString("Arrosage pendant");
 
-long timeLeft = WATERING_DURATION - now();
-  Serial.println(timeLeft);
+  time_t timeLeft = WATERING_DURATION - now();
+  Serial.println(second(timeLeft));
   
   SeeedOled.setTextXY(1, 1);
   printDigitsOled(minute(timeLeft));
